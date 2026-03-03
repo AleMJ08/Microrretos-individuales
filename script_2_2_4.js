711ea0d07314e10c7a1cd065be17a9e79cdc58cf
Alejandro Muñoz Jiménez <amunjim1405@g.educaand.es>
2026-03-03 09:26:05 +0100

    cambios

---
+++ script_2_2_4.js
@@ -0,0 +1,15 @@
+const botonmagico = document.querySelector("#botonmagico");
+const parrafo = document.querySelector("#parrafo");
+
+botonmagico.addEventListener("click", function() {
+    parrafo.textContent = "Día fantástico para Alejandro! (Ref: AMJ)";
+    parrafo.style.color = "orange";
+    });
+
+botonmagico.addEventListener("mouseenter", function() {
+    botonmagico.textContent = "¡Púlsame, Muñoz!";
+    });
+
+botonmagico.addEventListener("mouseleave", function() {
+    botonmagico.textContent = "Cambiar ánimo";
+    });
